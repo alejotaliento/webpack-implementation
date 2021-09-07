4a02b9946a8e0a9a44a721122a3aebccdcd3c413
Alejo Taliento <alee.taliento@gmail.com>
2021-09-07 12:43:02 -0300

    (fix): change urls

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,7 +1,7 @@
-import getData from '@utils/getData.js';
-import github from '@images/github.png';
-import twitter from '@images/twitter.png';
-import instagram from '@images/instagram.png';
+import getData from "@utils/getData.js";
+import github from "@images/github.png";
+import twitter from "@images/twitter.png";
+import instagram from "@images/instagram.png";
 
 const Template = async () => {
   const data = await getData();
@@ -26,10 +26,10 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https:/twitter.com/alejotaliento">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/alejotaliento">
             <img src="${github}" />
           </a>
           <a href="https://instagram.com/gndx">
